7a6683a5f9fe384c2537ce0abac328bc92200b9c
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2021-06-06 22:38:58 +0900

    Reset everything

--- src/main/java/io/github/twosquirrels/squirrelwallets/SquirrelWallets.java
+++ src/main/java/io/github/twosquirrels/squirrelwallets/SquirrelWallets.java
@@ -7,11 +7,12 @@ public final class SquirrelWallets extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Enabled SquirrelWallets");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Disabled SquirrelWallets");
     }
 }
