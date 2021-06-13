1a0e2182b3c13448741cbec3bff1daa3eb1996ac
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2021-06-14 00:02:17 +0900

    Update build.gradle
    Made it possible to get class comments

--- src/main/java/io/github/twosquirrels/squirrelwallets/SquirrelWallets.java
+++ src/main/java/io/github/twosquirrels/squirrelwallets/SquirrelWallets.java
@@ -5,9 +5,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 /**
  * SquirrelWallets
  * @version 1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
  * @author TwoSquirrels
  */
 public final class SquirrelWallets extends JavaPlugin {
+
+    /**
+     * TestClass
+     * @since 1.0-SNAPSHOT
+     * @author TwoSquirrels
+     */
+    private static class TestClass {
+        private static class TestClass2 {
+
+        }
+    }
 
     @Override
     public void onEnable() {
